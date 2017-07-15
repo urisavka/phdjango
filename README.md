d19066107910041b07a5445c9e4c30b70f153761
Yurii Savka <urisavka@gmail.com>
2017-07-15 21:01:23 +0200

    models v2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # phdjango
 pip install -r requirements.txt --no-index
+
+python manage.py runserver
+python manage.py makemigrations models
+python manage.py migrate
--- phdjango/models/models.py
+++ phdjango/models/models.py
@@ -1,58 +1,9 @@
+# coding=utf-8
 from django.db import models
 
 
-class ModelConfig(models.Model):
-    # general fields
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    title = models.CharField(null=True, max_length=1024)
-    # step 1
-    production_firm_structure_id = models.ForeignKey(FirmStructure, on_delete=models.CASCADE, null = True)
-    raw_firm_structure_id = models.ForeignKey(FirmStructure, on_delete=models.CASCADE, null=True)
-    capital_firm_structure_id = models.ForeignKey(FirmStructure, on_delete=models.CASCADE, null=True)
-
-    household_structure_id = models.ForeignKey(HouseholdStructure, on_delete=models.CASCADE, null = True)
-
-    government_structure_id = models.ForeignKey(GovernmentStructure, on_delete=models.CASCADE, null = True)
-    outside_world = models.BooleanField("Зовнішній світ", default=False)
-
-
-    def __str__(self):
-        return "Модель " + str(self.title) +"створена " + self.created_at.strftime("%Y-%m-%d %H:%M:%S")
-
-
-class FirmStructure(models.Model):
-    type = models.CharField("Тип фірми", choices = (
-        ('raw_firm', 'Виробник сировини'),
-        ('capital_firm', 'Виробник капіталу'),
-        ('production_firm', 'Виробник споживчої продукції')
-    ), default = 'production_firm')
-    salary = models.BooleanField("Заробітна платна", default=False)
-    price = models.BooleanField("Ціна", default=False)
-    workers_count = models.BooleanField("Планова кількість працівників", default=False)
-    plan = models.BooleanField("Плановий обсяг виробництва", default=False)
-    salary_budget = models.BooleanField("Бюджет на оплату праці", default=False)
-
-    raw_need = models.BooleanField("Потреба в сировині", default=False)
-    raw_budget = models.BooleanField("Бюджет на закупівлю сировини", default=False)
-
-    capital_need = models.BooleanField("Потреба в капіталі", default=False)
-    capital_budget = models.BooleanField("Бюджет на закупівлю капіталу", default=False)
-
-class HouseholdStructure(models.Model):
-    consumption_need = models.BooleanField("Потреба в споживчій продукції", default=False)
-    consumption_budget = models.BooleanField("Бюджет на споживання", default=False)
-
-class GovernmentStructure(models.Model):
-    income_tax = models.BooleanField("Податок на доходи фізичних осіб", default=False)
-    profit_tax = models.BooleanField("Податок на прибуток", default=False)
-    import_tax = models.BooleanField("Ввізне мито", default=False)
-
-    coefficient_help = models.BooleanField("Коефіцієнт розрахунку допомоги по безробіттю", default=False)
-    minimal_tax = models.BooleanField("Мінімальна допомога по безробіттю", default=False)
-
 class Learning(models.Model):
-    firm_structure_id = models.ForeignKey(FirmStructure, on_delete=models.CASCADE, null = True)
-    method = models.CharField("Метод навчання", choices = (
+    method = models.CharField("Метод навчання", choices=(
         ('intuitive', 'Інтуїтивний метод'),
         ('extrapolation', 'Метод екстраполяції тенденції'),
         ('moses', 'Метод маржі прибутку'),
@@ -69,7 +20,60 @@ class Learning(models.Model):
         ('perceptron', 'Одношаровий персептрон'),
         ('svm', 'Система опорних векторів'),
         ('classification_decision_tree', 'Класифікаційне дерево рішень'),
-    ), default = 'random')
+    ), default='random', max_length=1024)
+    count = models.IntegerField("Кількість фірм такого типу")
+
+
+class HouseholdStructure(models.Model):
+    consumption_need = models.BooleanField("Потреба в споживчій продукції", default=False)
+    consumption_budget = models.BooleanField("Бюджет на споживання", default=False)
+
+
+class FirmStructure(models.Model):
+    type = models.CharField("Тип фірми", choices=(
+        ('raw_firm', 'Виробник сировини'),
+        ('capital_firm', 'Виробник капіталу'),
+        ('production_firm', 'Виробник споживчої продукції')
+    ), default='production_firm', max_length = 1024)
+    salary = models.BooleanField("Заробітна платна", default=False)
+    price = models.BooleanField("Ціна", default=False)
+    workers_count = models.BooleanField("Планова кількість працівників", default=False)
+    plan = models.BooleanField("Плановий обсяг виробництва", default=False)
+    salary_budget = models.BooleanField("Бюджет на оплату праці", default=False)
+
+    raw_need = models.BooleanField("Потреба в сировині", default=False)
+    raw_budget = models.BooleanField("Бюджет на закупівлю сировини", default=False)
+
+    capital_need = models.BooleanField("Потреба в капіталі", default=False)
+    capital_budget = models.BooleanField("Бюджет на закупівлю капіталу", default=False)
+    learning = models.ManyToManyField(Learning)
+
+
+class GovernmentStructure(models.Model):
+    income_tax = models.BooleanField("Податок на доходи фізичних осіб", default=False)
+    profit_tax = models.BooleanField("Податок на прибуток", default=False)
+    import_tax = models.BooleanField("Ввізне мито", default=False)
+
+    coefficient_help = models.BooleanField("Коефіцієнт розрахунку допомоги по безробіттю", default=False)
+    minimal_tax = models.BooleanField("Мінімальна допомога по безробіттю", default=False)
+
+
+class ModelConfig(models.Model):
+    # general fields
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    title = models.CharField(null=True, max_length=1024)
+    # step 1
+    production_firm_structure_id = models.ForeignKey(FirmStructure, related_name="ProductionFirm", null=True)
+    raw_firm_structure_id = models.ForeignKey(FirmStructure, related_name="RawFirm", null=True)
+    capital_firm_structure_id = models.ForeignKey(FirmStructure, related_name="CapitalFirm", null=True)
+
+    household_structure_id = models.ForeignKey(HouseholdStructure, null=True)
+
+    government_structure_id = models.ForeignKey(GovernmentStructure, null=True)
+    outside_world = models.BooleanField("Зовнішній світ", default=False)
+
+    def __str__(self):
+        return "Модель " + str(self.title) + "створена " + self.created_at.strftime("%Y-%m-%d %H:%M:%S")
 
 
 class ModelRunConfiguration(models.Model):
@@ -81,8 +85,8 @@ class ModelRunConfiguration(models.Model):
 
 
 class ModelResult(models.Model):
-    modelRunConfiguration = models.ForeignKey(ModelRunConfiguration, on_delete=models.CASCADE, null=True)
-    modelConfig = models.ForeignKey(ModelConfig, on_delete=models.CASCADE, null=True)
+    modelRunConfiguration = models.ForeignKey(ModelRunConfiguration, null=True)
+    modelConfig = models.ForeignKey(ModelConfig, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
